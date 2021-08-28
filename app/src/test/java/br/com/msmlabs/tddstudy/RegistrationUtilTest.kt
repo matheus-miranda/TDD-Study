6192e8887323a0bc05472e4f24ea2ab2687931e1
matheus-miranda <matheus.sm86@gmail.com>
2021-08-27 22:14:24 -0300

    Write all test cases

--- app/src/test/java/br/com/msmlabs/tddstudy/RegistrationUtilTest.kt
+++ app/src/test/java/br/com/msmlabs/tddstudy/RegistrationUtilTest.kt
@@ -14,4 +14,54 @@ class RegistrationUtilTest {
         )
         assertThat(result).isFalse()
     }
+
+    @Test
+    fun `valid username and correctly repeated password returns true`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            username = "Jack",
+            password = "123",
+            confirmPassword = "123"
+        )
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `username already exists returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            username = "Peter",
+            password = "123",
+            confirmPassword = "123"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `password is empty returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            username = "Peter",
+            password = "",
+            confirmPassword = ""
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `password is not the same as confirmation returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            username = "Peter",
+            password = "123",
+            confirmPassword = "456"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `password has less than two digits returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            username = "Peter",
+            password = "abcd1",
+            confirmPassword = "abcd1"
+        )
+        assertThat(result).isFalse()
+    }
 }
